ed1ed1fd839314139c66d332d86ebd54342730b0
Jort van Waes <jortrr@pm.me>
2025-09-15 20:33:22 +0200

    feat: added command line arguments to select an aoc day

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,34 @@ mod parse;
 mod solver;
 
 use crate::day::*;
-use crate::solver::Solver;
+use crate::solver::{Int, Solver};
+use clap::Parser;
 use std::error::Error;
 
+#[derive(Parser)]
+#[command(name = "aoc24")]
+#[command(about = "Advent of Code 2024 - jortrr", long_about = None)]
+struct Cli {
+    /// Day to run, one of {1, ..., 25}
+    #[arg(short, long)]
+    day: Int,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let d01 = d01::Solver::run()?;
-    dbg!(d01);
-    let d02 = d02::Solver::run()?;
-    dbg!(d02);
-    let d03 = d03::Solver::run()?;
-    dbg!(d03);
-    let d04 = d04::Solver::run()?;
-    dbg!(d04);
-    let d05 = d05::Solver::run()?;
-    dbg!(d05);
+    let cli = Cli::parse();
+    println!(
+        "{:#?}",
+        match cli.day {
+            1 => d01::Solver::run()?,
+            2 => d02::Solver::run()?,
+            3 => d03::Solver::run()?,
+            4 => d04::Solver::run()?,
+            5 => d05::Solver::run()?,
+            _ => panic!(
+                "Day not supported: {}",
+                cli.day
+            ),
+        }
+    );
     Ok(())
 }
